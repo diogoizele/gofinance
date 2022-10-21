b803d4a29fd9125d5d13df60f2668526644c0e4e
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-21 17:52:40 -0300

    feat(fonts): load fonts in app and add trick to non render app if fonts not loaded

--- app.tsx
+++ app.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 import { Dashboard } from "./src/screens/Dashboard";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
-  return <Dashboard />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
