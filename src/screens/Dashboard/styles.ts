0afacc9fd31ae9418b88df7a20524506e59cf432
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-24 15:57:33 -0300

    feat: create custom type to styled flat list

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from "@expo/vector-icons";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from "react-native-iphone-x-helper";
+import { DataListProps } from ".";
+import { FlatList } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
@@ -14,8 +19,6 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
 
   background-color: ${({ theme }) => theme.colors.primary};
-
-  /* justify-content: center; */
   align-items: center;
 `;
 
@@ -74,4 +77,31 @@ export const HightLightCards = styled.ScrollView.attrs({
 
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1%;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  margin-bottom: 16px;
+
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})`
+  flex: 1%;
 `;
