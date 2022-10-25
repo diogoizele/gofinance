2ead5408f12c9aa2d05f8d1f1a78d2e53c75d4f9
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-25 10:48:19 -0300

    feat(register): start register new item screen

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../../components/Forms/Button";
+import { Input } from "../../components/Forms/Input";
+import { Container, Fields, Form, Header, Title } from "./styles";
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
+        <Button>Enviar</Button>
+      </Form>
+    </Container>
+  );
+}
