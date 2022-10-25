44701a9938567c4dd90dd07d2bd25a2b326dd3e2
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-25 10:48:36 -0300

    feat(input): create text input component

---
+++ src/components/Forms/Input/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from "./styles";
+import { TextInputProps } from "react-native";
+
+interface Props extends TextInputProps {}
+
+export function Input({ ...rest }: Props) {
+  return <Container {...rest}></Container>;
+}
