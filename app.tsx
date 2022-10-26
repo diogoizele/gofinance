2d8ae0914db9b37088c46358247786a0b1572232
diogoizele <diogogabrielizele@gmail.com>
2022-10-26 11:20:39 -0300

    feat(app): add app routes component to control app routing

--- app.tsx
+++ app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from "@react-navigation/native";
 
 import {
   useFonts,
@@ -8,9 +9,8 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 
-import { Dashboard } from "./src/screens/Dashboard";
 import theme from "./src/global/styles/theme";
-import { Register } from "./src/screens/Register";
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,8 +25,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
