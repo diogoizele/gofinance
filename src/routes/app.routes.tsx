ebf41282a6ab6b378e94c38bebd97cb157823532
diogoizele <diogogabrielizele@gmail.com>
2022-10-26 22:20:52 -0300

    feat(routes): configure screen options

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,25 +1,56 @@
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Feather } from "@expo/vector-icons";
+import { MaterialIcons as Icon } from "@expo/vector-icons";
 
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
+import { useTheme } from "styled-components";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const { colors } = useTheme();
+
   return (
     <Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: colors.secondary,
+        tabBarInactiveTintColor: colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+          height: 88,
+        },
       }}
     >
       <Screen
         name="Listagem"
         component={Dashboard}
-        options={{ tabBarIcon: () => <Feather name="activity" /> }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon color={color} name="format-list-bulleted" size={size} />
+          ),
+        }}
       />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon color={color} name="attach-money" size={size} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon color={color} name="pie-chart" size={size} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
